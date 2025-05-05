ad59578d9ee3763af60ad9890cf51536d3e8a53e
Timur Takhtarov <timtaktamu@tamu.edu>
2025-05-05 16:34:35 -0500

    docker stuff

--- offchain/client/exchange.py
+++ offchain/client/exchange.py
@@ -18,8 +18,6 @@ def main():
 
     args = parser.parse_args()
 
-    print("what the fuck please")
-
     if args.landlord: 
         landlord_pk = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
         landlord_sk = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
